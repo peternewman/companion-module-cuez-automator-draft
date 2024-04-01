3c4cc4298775e207757c7b39ac8d072bd4cbef00
Peter Newman <peterjnewman@gmail.com>
2024-04-01 16:03:42 +0100

    Add the simple presets

--- src/presets.js
+++ src/presets.js
@@ -9,7 +9,196 @@ module.exports = {
 		const foregroundColor = combineRgb(255, 255, 255) // White
 		const foregroundColorBlack = combineRgb(0, 0, 0) // Black
 		const backgroundColorRed = combineRgb(255, 0, 0) // Red
-		const backgroundColorGreen = combineRgb(0, 255, 0) // Red
+		const backgroundColorGreen = combineRgb(0, 255, 0) // Green
+		const backgroundColorYellow = combineRgb(255, 255, 0) // Yellow
+
+		presets['power_on'] = {
+			category: 'Power',
+			name: `Power On`,
+			type: 'button',
+			style: {
+				text: 'Power On',
+				size: '18',
+				color: foregroundColor,
+				bgcolor: foregroundColorBlack,
+			},
+			feedbacks: [
+				{
+					feedbackId: 'powerStatus',
+					style: {
+						bgcolor: backgroundColorYellow,
+						color: foregroundColorBlack,
+					},
+					options: {
+						state: true,
+					},
+				},
+			],
+			steps: [
+				{
+					down: [
+						{
+							actionId: 'power_on',
+							options: {},
+						},
+					],
+					up: [],
+				},
+			],
+		}
+
+		presets['power_off'] = {
+			category: 'Power',
+			name: `Power Off`,
+			type: 'button',
+			style: {
+				text: 'Power Off',
+				size: '18',
+				color: foregroundColor,
+				bgcolor: foregroundColorBlack,
+			},
+			feedbacks: [
+				{
+					feedbackId: 'powerStatus',
+					style: {
+						bgcolor: backgroundColorYellow,
+						color: foregroundColorBlack,
+					},
+					options: {
+						state: false,
+					},
+				},
+			],
+			steps: [
+				{
+					down: [
+						{
+							actionId: 'power_off',
+							options: {},
+						},
+					],
+					up: [],
+				},
+			],
+		}
+
+		presets['volume_mute'] = {
+			category: 'Volume',
+			name: `Mute`,
+			type: 'button',
+			style: {
+				text: 'Mute',
+				size: '18',
+				color: foregroundColor,
+				bgcolor: foregroundColorBlack,
+			},
+			feedbacks: [
+				{
+					feedbackId: 'muteStatus',
+					style: {
+						bgcolor: backgroundColorYellow,
+						color: foregroundColorBlack,
+					},
+					options: {
+						state: true,
+					},
+				},
+			],
+			steps: [
+				{
+					down: [
+						{
+							actionId: 'volume_mute',
+							options: {},
+						},
+					],
+					up: [],
+				},
+			],
+		}
+
+		presets['volume_unmute'] = {
+			category: 'Volume',
+			name: `Unmute`,
+			type: 'button',
+			style: {
+				text: 'Unmute',
+				size: '18',
+				color: foregroundColor,
+				bgcolor: foregroundColorBlack,
+			},
+			feedbacks: [
+				{
+					feedbackId: 'muteStatus',
+					style: {
+						bgcolor: backgroundColorYellow,
+						color: foregroundColorBlack,
+					},
+					options: {
+						state: false,
+					},
+				},
+			],
+			steps: [
+				{
+					down: [
+						{
+							actionId: 'volume_unmute',
+							options: {},
+						},
+					],
+					up: [],
+				},
+			],
+		}
+
+		presets['volume_up'] = {
+			category: 'Volume',
+			name: `Volume Up`,
+			type: 'button',
+			style: {
+				text: 'Volume Up',
+				size: '18',
+				color: foregroundColor,
+				bgcolor: foregroundColorBlack,
+			},
+			feedbacks: [],
+			steps: [
+				{
+					down: [
+						{
+							actionId: 'volume_up',
+							options: {},
+						},
+					],
+					up: [],
+				},
+			],
+		}
+
+		presets['volume_down'] = {
+			category: 'Volume',
+			name: `Volume Down`,
+			type: 'button',
+			style: {
+				text: 'Volume Down',
+				size: '18',
+				color: foregroundColor,
+				bgcolor: foregroundColorBlack,
+			},
+			feedbacks: [],
+			steps: [
+				{
+					down: [
+						{
+							actionId: 'volume_down',
+							options: {},
+						},
+					],
+					up: [],
+				},
+			],
+		}
 
 		self.setPresetDefinitions(presets)
 	},
